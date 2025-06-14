a310ea6e6606f7ada8ac5514f7e77ad4d713af85
Amisha Agarwal <amishaagarwal309@gmail.com>
2025-06-14 14:37:04 +0530

    Initial commit on main branch

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
+
 // Start with Main.java
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Todo List App!");
+        Scanner sc = new Scanner(System.in);
     }
 }
